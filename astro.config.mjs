67f352e0d06fc6b6c2ac7e68a8a690afc0172242
Luis Waldman <luiswaldman@gmail.com>
2026-06-03 16:23:57 -0300

    cambios astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robote.ar',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
