7fc7936bb2a47a15320079792226f1d6b3d58e9e
Luis Waldman <luiswaldman@gmail.com>
2026-06-03 16:27:51 -0300

    Actualizar dominio a rebote.ar y configurar workflow para rama master

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://robote.ar',
+  site: 'https://rebote.ar',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
