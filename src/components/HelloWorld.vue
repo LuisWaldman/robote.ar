c64bbaad5c13a791190be618378890b94bf0213a
Luis Waldman <luiswaldman@gmail.com>
2026-09-16 19:25:25 -0300

    fix build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import heroImg from '../assets/hero.png'
-import viteLogo from '../assets/vite.svg'
-import vueLogo from '../assets/vue.svg'
-
-const count = ref(0)
 </script>
 
 <template>
